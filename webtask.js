a60a77e7de842a48ac5371a81afd17164325b400
huunghiapn <huunghia.it11@gmail.com>
2017-04-19 17:12:03 +0700

    Update webtask.js - change persons group

--- webtask.js
+++ webtask.js
@@ -55,7 +55,7 @@ function identify(faceIds) {
             'Ocp-Apim-Subscription-Key': key
         },
         body: {
-            "personGroupId": 'vav-idols',
+            "personGroupId": 'splus-mems',
             "faceIds": faceIds,
             "maxNumOfCandidatesReturned": 1,
         },
